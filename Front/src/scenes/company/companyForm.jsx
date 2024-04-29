e52df345e86a2dc2e388384209470c7ca3618a52
idir15 <nechab.idir15@gmail.com>
2024-04-29 10:49:02 +0200

    first commit

---
+++ Front/src/scenes/company/companyForm.jsx
@@ -0,0 +1,196 @@
+import React from "react";
+import { useState } from 'react';
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
+import {
+  Button,
+  CardContent,
+  Grid,
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  ToggleButtonGroup,
+  ToggleButton,
+
+
+} from "@mui/material";
+
+const CompanyForm = ({ open, handleClose }) => {
+    const [alignment, setAlignment] = useState('esn');
+
+    const handleChange = (event, newAlignment) => {
+      if (newAlignment !== null) {
+        setAlignment(newAlignment);
+      }
+    };
+    const [formats, setFormats] = React.useState(() => ['bold', 'italic']);
+
+    const handleFormat = (event, newFormats) => {
+      setFormats(newFormats);
+    };
+  return (
+    <Dialog open={open} onClose={handleClose} fullWidth maxWidth="lg" sx={{
+        "& .MuiDialog-paper": {
+          width: "70%",
+          maxWidth: "none",
+          maxHeight: "70vh"
+        }
+      }}>
+
+      <DialogTitle sx={{ backgroundColor: "#0c54fa", color: "#fff" }}>
+        Nouvelle Compagnie
+      </DialogTitle>
+      <DialogContent>
+        <CardContent >
+          <br />
+          <label>
+          Type:
+          <ToggleButtonGroup
+      value={formats}
+      onChange={handleFormat}
+      aria-label="text formatting"
+      sx={{ marginBottom: '18px', width: '100%' }}
+    >
+      <ToggleButton value="italic" aria-label="italic"
+                sx={{
+                    width: '10%',
+                    fontSize: '18px',
+                  }}>Client
+        
+      </ToggleButton>
+      <ToggleButton value="underlined" aria-label="underlined"
+                sx={{
+                    width: '10%',
+                    fontSize: '18px',
+                  }}>
+        ESN
+      </ToggleButton>
+    </ToggleButtonGroup>
+        </label>
+
+
+<br />
+          <Grid container spacing={3} mb={4}>
+            <Grid item xs={6}>
+              <TextField
+               id="outlined-multiline-flexible"
+                label="Nom de l'entreprise"
+                placeholder="Nom de l'entreprise"
+                fullWidth
+                name="nom"
+                color="success"
+                sx={{ 
+                    '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': { fontSize: '18px' },
+                    '& .MuiInputLabel-root': { fontSize: '18px' },
+                    '& .MuiOutlinedInput-input': { padding: '14.5px 16px' } // Ajustez le padding selon vos besoins
+                  }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Adresse"
+                placeholder="Adresse"
+                fullWidth
+                name="adresse"
+                color="success"
+                sx={{ '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': { fontSize: '18px' } }}
+              />
+            </Grid>
+          </Grid>
+          <Grid container spacing={3} mb={4}>
+            <Grid item xs={6}>
+              <TextField
+                label="Nom de responsable"
+                placeholder="Nom de responsable"
+                fullWidth
+                name="nomResponsable"
+                color="success"
+                sx={{ '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': { fontSize: '18px' } }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Email"
+                placeholder="email@example.com"
+                fullWidth
+                type="email"
+                name="email"
+                color="success"
+                sx={{ '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': { fontSize: '18px' } }}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2} mb={4}>
+            <Grid item xs={4}>
+              <TextField
+                label="N° SIRET"
+                placeholder="Numero SIRET"
+                fullWidth
+                type="number"
+                name="siret"
+                color="success"
+                sx={{ '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': { fontSize: '18px' } }}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                label="Forme juridique"
+                placeholder="Forme juridique"
+                fullWidth
+                name="formeJuridique"
+                color="success"
+                sx={{ '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': { fontSize: '18px' } }}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                label="Code NAF"
+                placeholder="Code NAF"
+                fullWidth
+                name="codeNAF"
+                color="success"
+                sx={{ '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': { fontSize: '18px' } }}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <TextField
+                label="TVA"
+                placeholder="TVA Intra"
+                fullWidth
+                name="tva"
+                color="success"
+                sx={{ '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': { fontSize: '18px' } }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="TVA"
+                placeholder="TVA Intra"
+                fullWidth
+                name="tva"
+                color="success"
+                sx={{ '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': { fontSize: '18px' } }}
+              />
+            </Grid>
+          </Grid>
+        </CardContent>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="secondary">
+          Annuler
+        </Button>
+        <Button  color="primary">
+          Soumettre
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default CompanyForm;
---
+++ Front/src/scenes/company/index.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataContacts } from "../../data/mockData";
+import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
+import CompanyForm from "./companyForm";
+
+const Company = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", flex: 0.5 },
+    { field: "registrarId", headerName: "Registrar ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      flex: 1,
+    },
+    {
+      field: "Responsable",
+      headerName: "Responsable",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "N° SIRET",
+      headerName: "N° SIRET",
+      flex: 1,
+    },
+    {
+      field: "Forme juridique",
+      headerName: "Forme juridique",
+      flex: 1,
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "Code NAF",
+      headerName: "Code NAF",
+      flex: 1,
+    },
+    {
+      field: "TVA",
+      headerName: "TVA",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <>
+      <Box m="20px">
+        <Header
+          title="CONTACTS"
+          subtitle="List of Contacts for Future Reference"
+        />
+        <Button color="info" size="small" onClick={handleOpen}>
+          Ouvrir
+        </Button>
+        <div>
+          <CompanyForm open={open} handleClose={handleClose} />
+        </div>
+
+        <Box
+          m="40px 0 0 0"
+          height="75vh"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .name-column--cell": {
+              color: colors.greenAccent[300],
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: colors.blueAccent[400],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: colors.primary[400],
+            },
+            "& .MuiDataGrid-footerContainer": {
+              borderTop: "none",
+              backgroundColor: colors.blueAccent[700],
+            },
+            "& .MuiCheckbox-root": {
+              color: `${colors.greenAccent[200]} !important`,
+            },
+            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+              color: `${colors.grey[100]} !important`,
+            },
+          }}
+        >
+          <DataGrid
+            rows={mockDataContacts}
+            columns={columns}
+            components={{ Toolbar: GridToolbar }}
+          />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Company;
